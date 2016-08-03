83085d00ab6fd8a784729b045dbe6917181e17bf
Enigmaproject <angeljdj7@hotmail.com>
2016-08-03 16:20:13 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 (function () {
 
     // Change this to your GitHub username so you don't have to modify so many things.
-    var fork = "TyZeff";
+    var fork = "Chloe";
 
     // Define our function responsible for extending the bot.
     function extend() {
@@ -87,9 +87,9 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-      botName: "=^.°.^= MofChillout bot",
+      botName: "Chloe Muller",
       language: "english",
-      chatLink: "https://rawgit.com/" + fork + "/MoC-plugdj-Bot/master/lang/en.json",
+      chatLink: "https://rawgit.com/" + fork + "/Chloe-EPF-Plug.dj/master/lang/en.json",
       scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 1, // 1-200
